3d953adc0269e22a37d0872353de92706d22906b
david <david@DavidAvila>
2016-03-14 23:45:24 -0600

    "Agregue Nuevos datos a los usuarios y string"

--- src/inventarios/Productos.java
+++ src/inventarios/Productos.java
@@ -13,9 +13,66 @@ public class Productos {
     
     
     String producto;
+    
     String cantidad;
+    
     String caracteristicas;
+    
     String serial;
+     String tamaño;
+    
+    String color;
+    
+    String figura;
+    
+    String variedad;
+    
+    String cosas;
+    
+    String descripcion;
+    
+    String precio;
+    
+    String moneda;
+    
+    String calidad;
+    
+    String vistobueno;
+    
+    String cosa;
+    
+    String bueno;
+    
+    String malo;
+    
+    String regular;
+    
+    String sabor;
+    
+    String creado;
+    
+    String ensamblado;
+    
+    String facturado;
+    
+    String compra;
+    
+    String venta;
+    
+    String domicilio;
+    
+    String estado;
+    
+    String Ciudad;
+    
+    String Municipio;
+    
+    String Pueblo;
+   
+
+    
+    
+   
     
     public Productos(String producto,String cantidad,String caracteristicas,String serial){
         this.producto=producto;
@@ -55,7 +112,327 @@ public class Productos {
     public void setSerial(String serial) {
         this.serial = serial;
     }
+    
+    
+public String getTamaño() {
+        return tamaño;
+    }
 
+
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    
+    
+    public String getColor() {
+        return color;
+    }
+
+    
+    
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    
+    
+    public String getFigura() {
+        return figura;
+    }
+
+    
+    
+    public void setFigura(String figura) {
+        this.figura = figura;
+    }
+
+    
+    
+    
+    public String getVariedad() {
+        return variedad;
+    }
+
+    
+    
+    public void setVariedad(String variedad) {
+        this.variedad = variedad;
+    }
+
+    
+    
+    public String getCosas() {
+        return cosas;
+    }
+
+    
+    
+    
+    public void setCosas(String cosas) {
+        this.cosas = cosas;
+    }
+
+    
+    
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    
+    
+    
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    
+    
+    public String getPrecio() {
+        return precio;
+    }
+
+    
+    
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    
+    
+    public String getMoneda() {
+        return moneda;
+    }
+
+    
+    
+    public void setMoneda(String moneda) {
+        this.moneda = moneda;
+    }
+
+    
+    
+    
+    public String getCalidad() {
+        return calidad;
+    }
+
+    
+    
+    public void setCalidad(String calidad) {
+        this.calidad = calidad;
+    }
+
+    
+    
+    public String getVistobueno() {
+        return vistobueno;
+    }
+
+    
+    
+    public void setVistobueno(String vistobueno) {
+        this.vistobueno = vistobueno;
+    }
+
+    
+    
+    public String getCosa() {
+        return cosa;
+    }
+
+    
+    
+    public void setCosa(String cosa) {
+        this.cosa = cosa;
+    }
+
+    
+    
+    
+    public String getBueno() {
+        return bueno;
+    }
+
+    
+    
+    
+    public void setBueno(String bueno) {
+        this.bueno = bueno;
+    }
+
+    
+    
+    public String getMalo() {
+        return malo;
+    }
+
+    
+    
+    public void setMalo(String malo) {
+        this.malo = malo;
+    }
+
+    
+    
+    public String getRegular() {
+        return regular;
+    }
+
+    
+    
+    
+    public void setRegular(String regular) {
+        this.regular = regular;
+    }
+
+    
+    
+    
+    public String getSabor() {
+        return sabor;
+    }
+
+    
+    
+    
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    
+    
+    
+    public String getCreado() {
+        return creado;
+    }
+
+    
+    
+    public void setCreado(String creado) {
+        this.creado = creado;
+    }
+
+    
+    
+    public String getEnsamblado() {
+        return ensamblado;
+    }
+
+    
+    
+    public void setEnsamblado(String ensamblado) {
+        this.ensamblado = ensamblado;
+    }
+
+    
+    
+    public String getFacturado() {
+        return facturado;
+    }
+
+    
+    
+    public void setFacturado(String facturado) {
+        this.facturado = facturado;
+    }
+
+    
+    
+    
+    public String getCompra() {
+        return compra;
+    }
+
+    
+    
+    
+    public void setCompra(String compra) {
+        this.compra = compra;
+    }
+
+    
+    
+    
+    public String getVenta() {
+        return venta;
+    }
+
+    
+    
+    public void setVenta(String venta) {
+        this.venta = venta;
+    }
+
+    
+    
+    
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    
+    
+    
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    
+    
+    
+    public String getEstado() {
+        return estado;
+    }
+
+    
+    
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    
+    
+    
+    public String getCiudad() {
+        return Ciudad;
+    }
+
+    
+    
+    
+    public void setCiudad(String Ciudad) {
+        this.Ciudad = Ciudad;
+    }
+
+    
+    
+    
+    public String getMunicipio() {
+        return Municipio;
+    }
+
+    
+    
+    public void setMunicipio(String Municipio) {
+        this.Municipio = Municipio;
+    }
+
+    
+    
+    public String getPueblo() {
+        return Pueblo;
+    }
+
+    
+    
+    public void setPueblo(String Pueblo) {
+        this.Pueblo = Pueblo;
+    }
+    
+    
+    
         
         
     
--- src/inventarios/Usuarios.java
+++ src/inventarios/Usuarios.java
@@ -20,22 +20,174 @@ public class Usuarios {
         
        ArrayList<UsuariosParaInicioSecion> lista_usuarios = new ArrayList<>();
        //crear user//
-       UsuariosParaInicioSecion oscar = new UsuariosParaInicioSecion("oscar", "oscar");
-       //agregar a lista//
+      UsuariosParaInicioSecion francisco = new UsuariosParaInicioSecion("francisco", "francisco");
+       lista_usuarios.add(francisco);
+       
+UsuariosParaInicioSecion oscar = new UsuariosParaInicioSecion("oscar", "oscar");
        lista_usuarios.add(oscar);
        
-       UsuariosParaInicioSecion ignacio = new UsuariosParaInicioSecion("ignacio", "ignacio");
+UsuariosParaInicioSecion ignacio = new UsuariosParaInicioSecion("ignacio", "ignacio");
        lista_usuarios.add(ignacio);
+
        
-       UsuariosParaInicioSecion marroquin = new UsuariosParaInicioSecion("marroquin", "marroquin");
+UsuariosParaInicioSecion marroquin = new UsuariosParaInicioSecion("marroquin", "marroquin");
        lista_usuarios.add(marroquin);
        
-       UsuariosParaInicioSecion morales = new UsuariosParaInicioSecion("morales", "morales");
+       
+UsuariosParaInicioSecion morales = new UsuariosParaInicioSecion("morales", "morales");
        lista_usuarios.add(morales);
        
-        UsuariosParaInicioSecion francisco = new UsuariosParaInicioSecion("francisco", "francisco");
-       lista_usuarios.add(francisco);
-      
+       
+UsuariosParaInicioSecion ballina = new UsuariosParaInicioSecion("ballina", "ballina");
+       lista_usuarios.add(ballina);
+       
+       
+UsuariosParaInicioSecion erik = new UsuariosParaInicioSecion("erik", "erik");
+       lista_usuarios.add(erik);
+       
+           
+UsuariosParaInicioSecion david = new UsuariosParaInicioSecion("david", "david");
+       lista_usuarios.add(david);
+       
+       
+       
+UsuariosParaInicioSecion avila = new UsuariosParaInicioSecion("avila", "avila");
+       lista_usuarios.add(avila);
+       
+       
+UsuariosParaInicioSecion cruz = new UsuariosParaInicioSecion("cruz", "cruz");
+       lista_usuarios.add(cruz);
+       
+       
+       
+UsuariosParaInicioSecion rosa = new UsuariosParaInicioSecion("rosa", "rosa");
+       lista_usuarios.add(rosa);
+       
+       
+UsuariosParaInicioSecion arcos = new UsuariosParaInicioSecion("arcos", "arcos");
+       lista_usuarios.add(arcos);
+       
+          
+UsuariosParaInicioSecion vazquez = new UsuariosParaInicioSecion("vazquez", "vazquez");
+       lista_usuarios.add(vazquez);
+       
+       
+UsuariosParaInicioSecion edgar = new UsuariosParaInicioSecion("edgar", "edgar");
+       lista_usuarios.add(edgar);
+       
+       
+UsuariosParaInicioSecion gomez = new UsuariosParaInicioSecion("gomez", "gomez");
+       lista_usuarios.add(gomez);
+       
+       
+UsuariosParaInicioSecion fernando = new UsuariosParaInicioSecion("fernando", "fernando");
+       lista_usuarios.add(fernando);
+       
+       
+       
+UsuariosParaInicioSecion jimenez = new UsuariosParaInicioSecion("jimenez", "jimenez");
+       lista_usuarios.add(jimenez);
+       
+       
+UsuariosParaInicioSecion yesica = new UsuariosParaInicioSecion("yesica", "yesica");
+       lista_usuarios.add(yesica);
+       
+       
+UsuariosParaInicioSecion irene = new UsuariosParaInicioSecion("irene", "irene");
+       lista_usuarios.add(irene);
+       
+       
+UsuariosParaInicioSecion guadalupe = new UsuariosParaInicioSecion("guadalupe", "guadalupe");
+       lista_usuarios.add(guadalupe);
+       
+       
+UsuariosParaInicioSecion felix = new UsuariosParaInicioSecion("felix", "felix");
+       lista_usuarios.add(felix);
+       
+       
+UsuariosParaInicioSecion hernandez = new UsuariosParaInicioSecion("hernandez", "hernandez");
+       lista_usuarios.add(hernandez);
+
+       
+UsuariosParaInicioSecion xander = new UsuariosParaInicioSecion("xander", "xander");
+       lista_usuarios.add(xander);
+       
+       
+UsuariosParaInicioSecion xero = new UsuariosParaInicioSecion("xero", "xero");
+       lista_usuarios.add(xero);
+       
+       
+UsuariosParaInicioSecion trinidad = new UsuariosParaInicioSecion("trinidad", "trinidad");
+       lista_usuarios.add(trinidad);
+       
+       
+UsuariosParaInicioSecion montero = new UsuariosParaInicioSecion("montero", "montero");
+       lista_usuarios.add(montero);
+               
+               
+UsuariosParaInicioSecion aquino = new UsuariosParaInicioSecion("aquino", "aquino");
+       lista_usuarios.add(aquino);
+       
+       
+UsuariosParaInicioSecion lopez = new UsuariosParaInicioSecion("lopez", "lopez");
+       lista_usuarios.add(lopez);
+       
+       
+UsuariosParaInicioSecion marriano = new UsuariosParaInicioSecion("marriano", "marriano");
+       lista_usuarios.add(marriano);
+       
+       
+UsuariosParaInicioSecion miguel = new UsuariosParaInicioSecion("miguel", "miguel");
+       lista_usuarios.add(miguel);
+       
+       
+UsuariosParaInicioSecion lupita = new UsuariosParaInicioSecion("lupita", "lupita");
+       lista_usuarios.add(lupita);
+       
+       
+UsuariosParaInicioSecion carlos = new UsuariosParaInicioSecion("carlos", "carlos");
+       lista_usuarios.add(carlos);
+       
+       
+UsuariosParaInicioSecion alberto = new UsuariosParaInicioSecion("alberto", "alberto");
+       lista_usuarios.add(alberto);
+       
+       
+UsuariosParaInicioSecion tiburcio = new UsuariosParaInicioSecion("tiburcio", "tiburcio");
+       lista_usuarios.add(tiburcio);
+       
+       
+
+UsuariosParaInicioSecion dominga = new UsuariosParaInicioSecion("dominga", "dominga");
+       lista_usuarios.add(dominga);
+       
+       
+UsuariosParaInicioSecion torres = new UsuariosParaInicioSecion("torres", "torres");
+       lista_usuarios.add(torres);
+       
+       
+UsuariosParaInicioSecion moreno = new UsuariosParaInicioSecion("moreno", "moreno");
+       lista_usuarios.add(moreno);
+       
+       
+UsuariosParaInicioSecion pedro = new UsuariosParaInicioSecion("pedro", "pedro");
+       lista_usuarios.add(pedro);
+       
+       
+UsuariosParaInicioSecion sagrario = new UsuariosParaInicioSecion("sagrario", "sagrario");
+       lista_usuarios.add(sagrario);
+       
+       
+UsuariosParaInicioSecion heber = new UsuariosParaInicioSecion("heber", "heber");
+       lista_usuarios.add(heber);
+       
+       
+UsuariosParaInicioSecion olivera = new UsuariosParaInicioSecion("olivera", "olivera");
+       lista_usuarios.add(olivera);
+
+       
+       
+
        
        InicioSecionVentana login = new InicioSecionVentana (lista_usuarios);
        login.setVisible(true);
