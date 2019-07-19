518146fd51741430b2b00a07ffa3fca407c6da7b
Javatlacati <scherzo_16@hotmail.com>
2019-07-18 19:07:20 -0500

    improved variable names for the GUI for Login Window as part of issue #2

--- src/inventarios/InicioSecionVentana.java
+++ src/inventarios/InicioSecionVentana.java
@@ -68,91 +68,89 @@ public class InicioSecionVentana extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jPasswordField1 = new javax.swing.JPasswordField();
-        jTextField1 = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
+        btnAccept = new javax.swing.JButton();
+        btnClean = new javax.swing.JButton();
+        lblUser = new javax.swing.JLabel();
+        lblPassword = new javax.swing.JLabel();
+        txtPsswd = new javax.swing.JPasswordField();
+        txtUser = new javax.swing.JTextField();
+        lblTitle = new javax.swing.JLabel();
+        lblCorporativeIcon = new javax.swing.JLabel();
+        lblBackground = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Inicio Seccion");
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         setMaximizedBounds(new java.awt.Rectangle(0, 0, 0, 0));
-        setMaximumSize(new java.awt.Dimension(500, 400));
         setMinimumSize(new java.awt.Dimension(500, 400));
-        setPreferredSize(new java.awt.Dimension(500, 400));
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgLetras/aceptar.png"))); // NOI18N
-        jButton1.setBorder(null);
-        jButton1.setContentAreaFilled(false);
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnAccept.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgLetras/aceptar.png"))); // NOI18N
+        btnAccept.setBorder(null);
+        btnAccept.setContentAreaFilled(false);
+        btnAccept.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btnAccept.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnAcceptActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 260, 115, 49));
+        getContentPane().add(btnAccept, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 260, 115, 49));
 
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgLetras/limpiar.png"))); // NOI18N
-        jButton2.setBorder(null);
-        jButton2.setContentAreaFilled(false);
-        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btnClean.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgLetras/limpiar.png"))); // NOI18N
+        btnClean.setBorder(null);
+        btnClean.setContentAreaFilled(false);
+        btnClean.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        btnClean.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btnCleanActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 260, -1, 51));
+        getContentPane().add(btnClean, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 260, -1, 51));
 
-        jLabel1.setFont(new java.awt.Font("Trebuchet MS", 1, 24)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(0, 0, 153));
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgLetras/usuario.png"))); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(35, 107, -1, -1));
+        lblUser.setFont(new java.awt.Font("Trebuchet MS", 1, 24)); // NOI18N
+        lblUser.setForeground(new java.awt.Color(0, 0, 153));
+        lblUser.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgLetras/usuario.png"))); // NOI18N
+        getContentPane().add(lblUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(35, 107, -1, -1));
 
-        jLabel2.setFont(new java.awt.Font("Trebuchet MS", 1, 24)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(0, 0, 153));
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgLetras/contraseña.png"))); // NOI18N
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(35, 163, -1, -1));
+        lblPassword.setFont(new java.awt.Font("Trebuchet MS", 1, 24)); // NOI18N
+        lblPassword.setForeground(new java.awt.Color(0, 0, 153));
+        lblPassword.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgLetras/contraseña.png"))); // NOI18N
+        getContentPane().add(lblPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(35, 163, -1, -1));
 
-        jPasswordField1.addKeyListener(new java.awt.event.KeyAdapter() {
+        txtPsswd.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
-                jPasswordField1KeyTyped(evt);
+                txtPsswdKeyTyped(evt);
             }
         });
-        getContentPane().add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 170, 134, -1));
+        getContentPane().add(txtPsswd, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 170, 134, -1));
 
-        jTextField1.addKeyListener(new java.awt.event.KeyAdapter() {
+        txtUser.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyTyped(java.awt.event.KeyEvent evt) {
-                jTextField1KeyTyped(evt);
+                txtUserKeyTyped(evt);
             }
         });
-        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 120, 134, -1));
+        getContentPane().add(txtUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 120, 134, -1));
 
-        jLabel3.setFont(new java.awt.Font("Tempus Sans ITC", 1, 36)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(204, 0, 0));
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgLetras/login.png"))); // NOI18N
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(178, 24, -1, -1));
+        lblTitle.setFont(new java.awt.Font("Tempus Sans ITC", 1, 36)); // NOI18N
+        lblTitle.setForeground(new java.awt.Color(204, 0, 0));
+        lblTitle.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgLetras/login.png"))); // NOI18N
+        getContentPane().add(lblTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(178, 24, -1, -1));
 
-        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgFondos/Imagen2.png"))); // NOI18N
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 80, 150, 180));
+        lblCorporativeIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgFondos/Imagen2.png"))); // NOI18N
+        getContentPane().add(lblCorporativeIcon, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 80, 150, 180));
 
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgFondos/fondo 1.png"))); // NOI18N
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(-2, -6, 500, 390));
+        lblBackground.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ImgFondos/fondo 1.png"))); // NOI18N
+        getContentPane().add(lblBackground, new org.netbeans.lib.awtextra.AbsoluteConstraints(-2, -6, 500, 390));
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnAcceptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAcceptActionPerformed
         // TODO add your handling code here:
-               String user = jTextField1.getText ();
-        String password = jPasswordField1.getText();
+               String user = txtUser.getText ();
+        String password = txtPsswd.getText();
          boolean encontrado = false;
         
         for (int i = 0; i < lista.size()-1; i++) {
@@ -173,27 +171,27 @@ public class InicioSecionVentana extends javax.swing.JFrame {
 
     private void PasswordActionPerformed(java.awt.event.ActionEvent evt) {                                         
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnAcceptActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void btnCleanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCleanActionPerformed
         // limpiar ventana
-        jTextField1.setText(null);
-        jPasswordField1.setText(null);
-    }//GEN-LAST:event_jButton2ActionPerformed
+        txtUser.setText(null);
+        txtPsswd.setText(null);
+    }//GEN-LAST:event_btnCleanActionPerformed
 
-    private void jTextField1KeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField1KeyTyped
+    private void txtUserKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtUserKeyTyped
         char cTeclaPresionada=evt.getKeyChar();
         if (cTeclaPresionada==KeyEvent.VK_ENTER){
-            jButton1.doClick();
+            btnAccept.doClick();
         }
-    }//GEN-LAST:event_jTextField1KeyTyped
+    }//GEN-LAST:event_txtUserKeyTyped
 
-    private void jPasswordField1KeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jPasswordField1KeyTyped
+    private void txtPsswdKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtPsswdKeyTyped
            char cTeclaPresionada=evt.getKeyChar();
         if (cTeclaPresionada==KeyEvent.VK_ENTER){
-            jButton1.doClick();
+            btnAccept.doClick();
         }
-    }//GEN-LAST:event_jPasswordField1KeyTyped
+    }//GEN-LAST:event_txtPsswdKeyTyped
 
     /**
      * @param args the command line arguments
@@ -232,14 +230,14 @@ public class InicioSecionVentana extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JButton btnAccept;
+    private javax.swing.JButton btnClean;
+    private javax.swing.JLabel lblBackground;
+    private javax.swing.JLabel lblCorporativeIcon;
+    private javax.swing.JLabel lblPassword;
+    private javax.swing.JLabel lblTitle;
+    private javax.swing.JLabel lblUser;
+    private javax.swing.JPasswordField txtPsswd;
+    private javax.swing.JTextField txtUser;
     // End of variables declaration//GEN-END:variables
 }
