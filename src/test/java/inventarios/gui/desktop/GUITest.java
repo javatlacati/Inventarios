99decb84577bacecd62945dcbebeb166eb9f3a30
Javatlacati <scherzo_16@hotmail.com>
2019-08-06 02:59:15 -0500

    starting with application migration to spring boot

--- src/test/java/inventarios/gui/desktop/GUITest.java
+++ src/test/java/inventarios/gui/desktop/GUITest.java
@@ -12,25 +12,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 import javax.swing.*;
 
 
-//@RunWith(MockitoJUnitRunner.class)
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ContextConfiguration(loader = HeadlessSpringBootContextLoader.class)
 public class GUITest {
 
-    //    @Mock
-//    private LoginUsersService usersService;
-//
-//    @InjectMocks
     @Autowired
-//    private ApplicationContext context;
     private LoginWindow loginWindow;
-//    private LoginWindow loginWindow = new LoginWindow();
 
     @Test
     public void main() {
-//        Mockito.when(usersService.findAll()).thenReturn(Collections.emptyList());
-//        LoginWindow loginWindow = context.getBean(LoginWindow.class);
         loginWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         loginWindow.setVisible(true);
         LoginWindowPageObject loginWindowPageObject = new LoginWindowPageObject();
