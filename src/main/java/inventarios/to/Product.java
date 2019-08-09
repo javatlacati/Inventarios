820e0ef9bd128cea7d9b37245d72785f05754280
Javatlacati <scherzo_16@hotmail.com>
2019-08-09 14:42:20 -0500

    modified product to be a database entity

--- src/main/java/inventarios/to/Product.java
+++ src/main/java/inventarios/to/Product.java
@@ -5,8 +5,14 @@
  */
 package inventarios.to;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
@@ -16,81 +22,82 @@ import lombok.Setter;
 @AllArgsConstructor
 @Setter
 @Getter
-public class Product {
-    
+@Entity
+@NoArgsConstructor
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+//   @GraphQLQuery(name = "id", description = "El identificador de producto")
+    private Long id;
+
     String Fechai;
-    
+
     String Fechae;
-    
+
     String producto;
-    
+
     String cantidad;
-    
+
     String caracteristicas;
-    
+
     String serial;
-     String size;
-    
+    String size;
+
     String color;
-    
+
     String figura;
-    
+
     String variedad;
-    
+
     String cosas;
-    
+
     String descripcion;
-    
+
     String precio;
-    
+
     String moneda;
-    
+
     String calidad;
-    
+
     String vistobueno;
-    
+
     String cosa;
-    
+
     String bueno;
-    
+
     String malo;
-    
+
     String regular;
-    
+
     String sabor;
-    
+
     String creado;
-    
+
     String ensamblado;
-    
+
     String facturado;
-    
+
     String compra;
-    
+
     String venta;
-    
+
     String domicilio;
-    
+
     String estado;
-    
+
     String Ciudad;
-    
+
     String Municipio;
-    
+
     String Pueblo;
-    
-    public Product(String producto,String cantidad,String caracteristicas,String serial, String fechai, String fechae){
-        this.producto=producto;
-        this.cantidad=cantidad;
-        this.caracteristicas=caracteristicas;
-        this.serial=serial;
-        this.Fechai=fechai;
-        this.Fechae=fechae;
+
+    public Product(String producto, String cantidad, String caracteristicas, String serial, String fechai, String fechae) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.caracteristicas = caracteristicas;
+        this.serial = serial;
+        this.Fechai = fechai;
+        this.Fechae = fechae;
     }
 }
-        
-        
-    
-    
-    
-
