0d24aa207c3ebfc3b8136a8fee4e0aa41bbd83a8
Javatlacati <scherzo_16@hotmail.com>
2019-08-09 14:42:59 -0500

    created a product service

---
+++ src/main/java/inventarios/service/ProductService.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventarios.service;
+
+import inventarios.repository.ProductRepository;
+import inventarios.to.Product;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Ivana Martinez C
+ */
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public <S extends Product> S save(S s) {
+        return productRepository.save(s);
+    }
+
+    public <S extends Product> Optional<S> findOne(Example<S> exmpl) {
+        return productRepository.findOne(exmpl);
+    }
+ 
+}
