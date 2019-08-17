035fd9ebbc2609a63972eb905f0cd39af04f193e
Javatlacati <scherzo_16@hotmail.com>
2019-08-17 00:00:13 -0500

    added method on service module to validate a login request

--- inventarios-service/src/main/java/inventarios/service/LoginUsersService.java
+++ inventarios-service/src/main/java/inventarios/service/LoginUsersService.java
@@ -3,6 +3,7 @@ package inventarios.service;
 import inventarios.repository.LoginUsersRepository;
 import inventarios.to.LoginUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,7 +13,14 @@ public class LoginUsersService {
     @Autowired
     LoginUsersRepository usersRepository;
 
-    public List<LoginUser> findAll(){
+    public List<LoginUser> findAll() {
         return usersRepository.findAll();
+    }
+
+    public final boolean login(LoginUser user) {
+        return usersRepository.
+                findOne(Example.of(user))
+                .map(LoginUser::isActive)
+                .orElse(Boolean.FALSE);
     }
 }
