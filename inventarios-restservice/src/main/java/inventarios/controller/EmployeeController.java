8c4d36fabd624d6c7010cbd7ea2f7468bbd7bf26
Javatlacati <scherzo_16@hotmail.com>
2019-08-16 20:11:32 -0500

    added module to provide a rest service

---
+++ inventarios-restservice/src/main/java/inventarios/controller/EmployeeController.java
@@ -0,0 +1,38 @@
+package inventarios.controller;
+
+import inventarios.service.EmployeeService;
+import inventarios.to.EmployeeDetail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    List<EmployeeDetail> all() {
+        return employeeService.findAll();
+    }
+
+    @PostMapping("/employees")
+    EmployeeDetail newEmployee(@RequestBody EmployeeDetail newEmployee) {
+        return employeeService.save(newEmployee);
+    }
+
+//    @GetMapping("/employees/{id}")
+//    EmployeeDetail one(@PathVariable Long id) {
+//
+//        return employeeService.findById(id)
+//                .orElseThrow(() -> new EmployeeNotFoundException(id));
+//    }
+}
