fd674e4e2dc46ad7ea674dff983351f4f9a8b673
javatlacati <scherzo_16@hotmail.com>
2019-08-31 12:08:54 -0500

    rescued frames from https://github.com/oscardamas/Inventarios

---
+++ inventarios-desktop/src/main/java/inventarios/desktop/RegistroClase.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2019 Ruslan López <scherzo16 at gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package inventarios.desktop;
+
+/**
+ *
+ * @author oscar
+ */
+public class RegistroClase {
+     public RegistroClase(String nombre, String codigo, String modelo, String categoria, String observaciones) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.modelo = modelo;
+        this.categoria = categoria;
+        this.observaciones = observaciones;
+    }
+
+  
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+    
+    private String nombre;
+    private String codigo;
+    private String modelo;
+    private String categoria;
+    private String observaciones;
+}
