8154afc2c8b59d577fb95f501b7e8902be5f2bf9
Javatlacati <scherzo_16@hotmail.com>
2019-09-06 10:13:53 -0500

    fixing unwired service at inventarios rest service

--- inventarios-restservice/src/main/java/inventarios/controller/BillingController.java
+++ inventarios-restservice/src/main/java/inventarios/controller/BillingController.java
@@ -2,13 +2,14 @@ package inventarios.controller;
 
 import inventarios.service.BillingService;
 import inventarios.to.BillingDetails;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class BillingController {
+    @Autowired
     private BillingService billingService;
 
     @GetMapping("/billing")
