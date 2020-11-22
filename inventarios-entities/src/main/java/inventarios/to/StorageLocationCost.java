6412295f4615c1ead8cec6651d40e22cf3460bf1
javatlacati <scherzo_16@hotmail.com>
2020-11-22 15:29:29 -0600

    added entity to track cost of the storage location with plans to see how it impacts the price of the product to the client

--- inventarios-entities/src/main/java/inventarios/to/StorageLocationCost.java
+++ inventarios-entities/src/main/java/inventarios/to/StorageLocationCost.java
@@ -16,19 +16,21 @@
  */
 package inventarios.to;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import lombok.EqualsAndHashCode;
-
+/**
+ *
+ * @author RuslanLopez
+ */
 @AllArgsConstructor
 @Setter
 @Getter
@@ -36,15 +38,9 @@ import lombok.EqualsAndHashCode;
 @NoArgsConstructor
 @ToString
 @EqualsAndHashCode
-public class StorageLocation {
+public class StorageLocationCost {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String locationName;
-    private String state;
-    private String city;
-//    String Municipio;
-//    String Pueblo;
-    @OneToOne
-    StorageLocationCost cost;
+    double montlyCost;
 }
